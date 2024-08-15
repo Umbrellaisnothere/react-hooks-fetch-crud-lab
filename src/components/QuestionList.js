c21b6987d66b2d8c74416934be44e89cc8d5789d
Keith Murimi <keith.murimi@student.moringaschool.com>
2024-08-15 14:55:25 +0300

    Initial commit

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,6 +1,30 @@
 import React from "react";
+import { useState, useEffect } from 'react';
+import QuestionItem from './QuestionItem';
 
 function QuestionList() {
+  const [questions, setQuestions] = useState([]);
+
+  useEffect(() => {
+    // Fetch questions from API
+    fetch('http://localhost:4000/questions')
+      .then(response => response.json())
+      .then(data => setQuestions(data))
+      .catch(error => console.error('Error fetching questions:', error));
+  }, []);
+
+  const questionItems = questions.map(question => (
+    <QuestionItem key={question.id} question={question} />
+  ));
+
+  return (
+    <section>
+      <h1>Quiz Questions</h1>
+      <ul>{questionItems}</ul>
+    </section>
+  );
+}
+
   return (
     <section>
       <h1>Quiz Questions</h1>
